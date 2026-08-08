7d9eae748b48ca48de89878710e3ea4461ed47c5
0609Abhinav <abhinavtripathi6sep.com>
2026-08-08 17:43:16 +0530

    Fix AOS horizontal scrollbars globally in base.html body tag

--- deploy_pa.py
+++ deploy_pa.py
@@ -9,6 +9,7 @@ headers = {'Authorization': f'Token {token}'}
 files_to_upload = [
     'books/templates/senior/index.html',
     'books/templates/senior/book_detail.html',
+    'books/templates/base.html',
     'books/static/css/global.css',
 ]
 
